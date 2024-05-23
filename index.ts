6dafa2b0dd7e371a31153046c427eb14590dfde0
jiangweiye <jiangwy@staryea.com>
2024-05-23 14:45:14 +0800

    feat: update

--- index.ts
+++ index.ts
@@ -2,6 +2,14 @@ import type { Options } from 'prettier';
 
 /**
  * @description prettier configuration
+ * @param {Options} options
+ * @author <jiangweiye@outlook.com>
+ * @example
+ * prettier({
+ *    printWidth: 140,
+ *   tabWidth: 4,
+ * })
+ * @see https://prettier.io/docs/en/options.html
  * @returns {Options}
  */
 function prettier(options?: Options): Options {
