959ec75e54f6ca267c01029b278b5461a91ca85f
jiangweiye <jiangwy@staryea.com>
2024-05-24 10:26:26 +0800

    docs: update

---
+++ dist/index.d.cts
@@ -0,0 +1,17 @@
+import { Options } from 'prettier';
+
+/**
+ * @description prettier configuration
+ * @param {Options} options
+ * @author <jiangweiye@outlook.com>
+ * @example
+ * prettier({
+ *   printWidth: 140,
+ *   tabWidth: 4,
+ * })
+ * @see https://prettier.io/docs/en/options.html
+ * @returns {Options} options
+ */
+declare function prettier(options?: Options): Options;
+
+export { prettier };
---
+++ dist/index.esm.js
@@ -0,0 +1,27 @@
+// index.ts
+function prettier(options) {
+  return {
+    printWidth: 140,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    quoteProps: "as-needed",
+    jsxSingleQuote: true,
+    trailingComma: "none",
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: "avoid",
+    insertPragma: false,
+    proseWrap: "never",
+    htmlWhitespaceSensitivity: "strict",
+    vueIndentScriptAndStyle: false,
+    endOfLine: "lf",
+    embeddedLanguageFormatting: "auto",
+    singleAttributePerLine: false,
+    ...options || {}
+  };
+}
+export {
+  prettier
+};
