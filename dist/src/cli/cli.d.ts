db452ec63676f1a691cee2a51bbe435db1390c15
jiangwy <jiangwy@staryea.com>
2024-10-15 10:38:49 +0800

    fix: update release yml

--- dist/src/cli/cli.d.ts
+++
@@ -1,9 +0,0 @@
-declare const fileType: {
-    label: string;
-    value: {
-        type: string;
-        fileName: string;
-    };
-}[];
-
-export { fileType };
--- dist/src/options.d.ts
+++
@@ -1,9 +0,0 @@
-import { Options } from 'prettier';
-
-/**
- * @description prettier default options
- * @see https://prettier.io/docs/en/options.html
- */
-declare const DEFAULT_OPTIONS: Options;
-
-export { DEFAULT_OPTIONS };
